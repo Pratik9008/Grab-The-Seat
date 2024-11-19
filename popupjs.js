768de0ec50fe890f016a6f8c999e46798d5fa462
sarthakkumar52 <anshusharma62021@gmail.com>
2024-11-19 21:15:13 +0530

    Add files via upload
    This project is a web-based application designed to simplify the process of booking movie theater seats. Users can easily browse and select available seats for a specific movie show through an intuitive interface. The website features a real-time visual representation of the theater's seating layout, clearly indicating available, booked, and selected seatsDeveloped using HTML, CSS, and JavaScript, the application provides a responsive and user-friendly experience across various devices. The front-end technologies ensure a smooth, interactive interface, making it convenient for users to book their seats from desktops or mobile devices. This project is ideal for enhancing the movie-going experience by streamlining seat reservations for both users and theater management.
    Contributions and feedback are welcome to help improve the application further!

---
+++ popupjs.js
@@ -0,0 +1,23 @@
+
+
+
+
+// const mybutton = document.getElementById("show-btn")
+
+// mybutton.addEventListener('click',function(){
+//     alert("Button Clicked!");
+
+// });
+
+
+
+{/* <script>JsBarcode("#barcode", "Hi world!");</script>
+
+<script>
+  const mybutton = document.getElementById("show-btn")
+
+  mybutton.addEventListener('click',function(){
+  alert("Tickets are now Booked");
+
+  });
+</script> */}
